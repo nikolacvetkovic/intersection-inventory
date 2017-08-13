c0e01e3de8275559b4be590e21a384abaea62ac3
cvele <nikola.cvetkovic91@gmail.com>
2017-08-13 23:02:48 +0200

    delete unused variable

--- src/main/java/com/intersections/config/SessionListener.java
+++ src/main/java/com/intersections/config/SessionListener.java
@@ -2,18 +2,12 @@ package com.intersections.config;
 
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-
 /**
  *
  * @author Cvele
  */
 public class SessionListener implements HttpSessionListener{
 
-    @Autowired
-    Environment env;
-    
     @Override
     public void sessionCreated(HttpSessionEvent se) {
         se.getSession().setMaxInactiveInterval(30*60);
